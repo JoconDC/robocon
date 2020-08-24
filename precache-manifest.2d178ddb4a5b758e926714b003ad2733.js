cd81c43ab7e6e0a1a0e7fa2240b6eda4db737e4d
Hesberto Concepcion <hesbertoconcepcion@Hesbertos-MacBook-Pro.local>
2020-08-24 12:29:57 -0400

    Updates

---
+++ precache-manifest.2d178ddb4a5b758e926714b003ad2733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f9b72d9fb201b91ded754c30878503",
+    "url": "/robocon/index.html"
+  },
+  {
+    "revision": "767f1cac64010e73e79b",
+    "url": "/robocon/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "82378fa95baa90523207",
+    "url": "/robocon/static/css/main.21382c13.chunk.css"
+  },
+  {
+    "revision": "767f1cac64010e73e79b",
+    "url": "/robocon/static/js/2.b4baa95b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robocon/static/js/2.b4baa95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82378fa95baa90523207",
+    "url": "/robocon/static/js/main.3aa11fab.chunk.js"
+  },
+  {
+    "revision": "1ec90ef193e079bbecef",
+    "url": "/robocon/static/js/runtime-main.9c131594.js"
+  }
+]);
